a851932e18455e47a871dc8f82f20edfb25cd5fa
NoorAthamny <athamnynoor@gmail.com>
2024-11-16 09:57:11 +0200

    initial setup / Vite / React Router / Html/Css / ReadMe File

---
+++ Vote-Project/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+// eslint-disable-next-line no-unused-vars
+import { BrowserRouter, Routes, Route , Switch } from "react-router-dom";
+function App() {
+
+
+  return (
+    <></>
+  )
+}
+
+export default App
