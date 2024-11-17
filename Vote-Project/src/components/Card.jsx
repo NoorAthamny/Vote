121a4a6067b67ef0fb45f44986ab180e4ccdbcab
NoorAthamny <athamnynoor@gmail.com>
2024-11-17 21:06:57 +0200

    vote logic / Vote Page

--- Vote-Project/src/components/Card.jsx
+++ Vote-Project/src/components/Card.jsx
@@ -1,9 +1,75 @@
+/* eslint-disable react/prop-types */
+import { useState , useEffect} from "react";
+const Card = ({ img , title} ) => {
+    
+    const [score, setScore] = useState(0);
+    const [voted, setVoted] = useState(false); 
+    const [showConfirmation, setShowConfirmation] = useState(false);
 
-const Card = () => {
+
+    useEffect(() => {
+        const storedScore = localStorage.getItem("voteScore");
+        if (storedScore && parseInt(storedScore, 10) > 0) {
+          setScore(parseInt(storedScore, 10));
+          setVoted(true); 
+        } else {
+            setVoted(false)
+        }
+      }, []);
+
+      const handleVote = () => {
+        setShowConfirmation(true);
+      };
+
+      const handleConfirm = () => {
+        const newScore = score + 1;
+        setScore(newScore);
+        localStorage.setItem("voteScore", newScore.toString());
+        setShowConfirmation(false);
+        setVoted(true);
+      };
+
+      const handleCancel = () => {
+        setShowConfirmation(false);
+      };
+
+      const handleChangeVote = () => {
+        setScore(0);
+        localStorage.removeItem("voteScore");
+        setVoted(false);
+        setShowConfirmation(false);
+      };
+
   return (
-    <article>
+
+   
+      
+            <section className="card">
+
+               <div className="img">
+                <img src={img} alt={title} />
+                </div>
+
+                <h3>{title}</h3>
+
+                {!voted && !showConfirmation &&(<button onClick={handleVote} className="vote" type="button">Vote</button>)}
+
+              { showConfirmation && (  <div>
+                <button onClick={handleConfirm} type="button">Im Sure</button>
+                <button onClick={handleCancel} type="button">Cancel</button>
+                </div >)}
+
+              { voted && !showConfirmation && (<div >
+                <button onClick={handleChangeVote} type="button">Change Vote</button>
+                </div>)}
+
+                <p className="vote-score">Vote: {score}</p>
+            </section>
+       
+
+
         
-    </article>
+    
   )
 }
 
--- Vote-Project/src/components/Yazan.jsx
+++ Vote-Project/src/components/Yazan.jsx
@@ -1,11 +1,39 @@
+import Card from "./Card"
 import NavBar from "./NavBar"
+import { useState } from "react"
 
 const Yazan = () => {
+
+    const [votedCardIndex, setVotedCardIndex] = useState(null);
+
   return (
-    <>
+    <section className="vote-page">
       <NavBar/>
-      
-    </>
+      <h1>Vote Page</h1>
+      <section className="cards">
+      <Card index={0} votedCardIndex={votedCardIndex} 
+      setVotedCardIndex={setVotedCardIndex}
+      title="Attack On Titan" img="images\Attack-on-Titan.jpg"/>
+
+      <Card 
+      index={1} 
+      votedCardIndex={votedCardIndex}
+      setVotedCardIndex={setVotedCardIndex}
+      title="Naruto" img="images\Naruto-Shippuden.jpg"/>
+
+      <Card 
+      index={2} 
+      votedCardIndex={votedCardIndex}
+      setVotedCardIndex={setVotedCardIndex}
+      title="One Piece" img="images\One-Piece.jpg"/>
+
+      <Card
+      index={3} 
+      votedCardIndex={votedCardIndex}
+      setVotedCardIndex={setVotedCardIndex}
+      title="Attack On Titan" img="images\Demon-Slayer.jpg"/>
+      </section>
+    </section>
   )
 }
 
